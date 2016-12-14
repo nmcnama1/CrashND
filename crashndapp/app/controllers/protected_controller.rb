cc1eeaa87b1f0b5d993ed6208b8b161ca480cbe4
Gaurav Sirdeshpande <gsirdesh@nd.edu>
2016-12-13 20:52:56 -0500

    Added set active user method

--- crashndapp/app/controllers/protected_controller.rb
+++ crashndapp/app/controllers/protected_controller.rb
@@ -1,10 +1,15 @@
 class ProtectedController < ApplicationController
   before_action :require_login
+  before_action :set_active_user
 
   def require_login
     unless session[:user_id]
       flash[:message] = "You must be logged in to view this."
       redirect_to login_path
     end
+  end
+
+  def set_active_user
+    @active_user = User.find(session[:user_id])
   end
 end
